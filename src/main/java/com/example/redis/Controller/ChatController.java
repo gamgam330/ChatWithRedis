89af82fd84931cce9103e883950227406cbfe042
gamgam330 <gamgam0330@naver.com>
2023-09-06 21:26:42 +0900

    add redis

--- src/main/java/com/example/redis/Controller/ChatController.java
+++ src/main/java/com/example/redis/Controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.example.redis.Controller;
 
 import com.example.redis.DTO.ChatMessage;
+import com.example.redis.Redis.RedisPublisher;
+import com.example.redis.Repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,27 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-// import 생략...
-
 @RequiredArgsConstructor
 @Controller
-@CrossOrigin
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
-
-    @MessageMapping("/info")
-    @SendTo("/your-info-channel")
-    public String getInfo() {
-        // 연결 정보를 생성하고 반환하는 로직 작성
-        return "Your connection info";
-    }
-
 }
